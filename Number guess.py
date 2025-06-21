fe98041df06bf3d1dab11b20f504ca385977bcb2
Geetansh <pydeveloperstudio@gmail.com>
2025-06-21 19:44:24 +0530

    Dice roller complete

---
+++ Number guess.py
@@ -0,0 +1,16 @@
+import random 
+max_attempts = 7
+
+for attempts in range(max_attempts):
+    num = random.randint(1,100)
+    user = int(input("Enter Your Guess: "))
+
+    if num == user:
+        print("Wow🎉, You won the Game!")
+        break
+    elif num >= user:
+        print("Chose higher number")
+    else:
+        print("Chose lower number")
+else:
+    print("You use your all attempts")
